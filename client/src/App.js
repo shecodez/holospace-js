73c3d9dfc43077c04ea86dc14962cc1a84158b67
shecodez - Nicole J. Nobles <shecodez@gmail.com>
2018-01-07 20:16:07 -0500

    Update c/s package, index files, and port

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h2>Big Cities</h2>
+        <h2>Big Ol Cities</h2>
         <ul>
           {this.state.cities.map( city => {
             return <li key={city.name}> <b>{city.name}</b>: {city.population}</li>
--- server/src/index.js
+++ server/src/index.js
@@ -8,14 +8,15 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 const router = express.Router()
 
-const staticFiles = express.static(path.join(__dirname, '../../client/build'))
+const staticFiles = express.static(path.join(__dirname, '../../../client/build'))
 app.use(staticFiles)
 
 router.get('/cities', (req, res) => {
   const cities = [
     {name: 'New York City', population: 8175133},
     {name: 'Los Angeles',   population: 3792621},
-    {name: 'Chicago',       population: 2695598}
+    {name: 'Chicago',       population: 2695598},
+    {name: 'Tokyo',         population: 9262046}
   ]
   res.json(cities)
 })
@@ -25,7 +26,7 @@ app.use(router)
 // any routes not picked up by the server api will be handled by the react router
 app.use('/*', staticFiles)
 
-app.set('port', (process.env.PORT || 3001))
+app.set('port', (process.env.PORT || 9001))
 app.listen(app.get('port'), () => {
   console.log(`Listening on ${app.get('port')}`)
 })
